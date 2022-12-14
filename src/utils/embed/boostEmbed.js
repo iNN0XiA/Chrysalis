1eff57e6cb564cc8b09402912f8aa8dbd5375c20
pLaYa iNN0XiA <118818738+iNN0XiA@users.noreply.github.com>
2022-12-14 01:56:20 -0500

    Update boostEmbed.js

--- src/utils/embed/boostEmbed.js
+++ src/utils/embed/boostEmbed.js
@@ -1,41 +1,15 @@
-module.exports = async (users, guild, guildInfo, lang) => {
-  let lb = ''
-  let highscores = users.sort((a, b) => (a.xp < b.xp) ? 1 : -1);
-  for (i in highscores.slice(0,10)) {
-    lb+=`${getNumberEmoji(+i+1)} ► <@${highscores[i].id}>
-          ${lang.level}: \`${Math.trunc((Math.sqrt(5)/5)*Math.sqrt(highscores[i].xp))}\`
-          XP: \`${highscores[i].xp}\`\n`;
-  }
-  return {
-    title: lang.leaderboard_title,
-    description: lb,
-    color: guildInfo.color,
-    thumbnail: { url: guild.iconURL({size:1024}) }
-  };
-}
+const { PermissionsBitField } = require('discord.js');
 
-
-function getNumberEmoji(n) {
-  switch (n) {
-    case 1:
-    return ':first_place:'
-    case 2:
-    return ':second_place:'
-    case 3:
-    return ':third_place:'
-    case 4:
-    return ':four:'
-    case 5:
-    return ':five:'
-    case 6:
-    return ':six:'
-    case 7:
-    return ':seven:'
-    case 8:
-    return ':eight:'
-    case 9:
-    return ':nine:'
-    case 10:
-    return ':keycap_ten:'
-  }
+module.exports = async (client, user, level, channelID, color, lang) => {
+	if (!channelID) return;
+	let channel = await client.channels.fetch(channelID).catch(r=>{});
+	if (!channel) return;
+	if (!channel.permissionsFor(client.user.id).has(PermissionsBitField.Flags.ViewChannel)) return;
+	if (!channel.permissionsFor(client.user.id).has(PermissionsBitField.Flags.SendMessages)) return;
+	channel.send({content: `${user}`, embeds:[{
+		title: user.username,
+		description: lang.levelup(level),
+		color: color,
+		thumbnail: { url: user.displayAvatarURL({ forceStatic: true, size: 512 }) }
+	}]}).catch(r=>{});
 }
