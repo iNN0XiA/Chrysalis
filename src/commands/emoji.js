13505eef7a2e658ab79cd4811f4d0e1cab15bba8
pLaYa iNN0XiA <118818738+iNN0XiA@users.noreply.github.com>
2022-12-14 01:35:20 -0500

    Update emoji.js

--- src/commands/emoji.js
+++ src/commands/emoji.js
@@ -1,20 +1,33 @@
+const fetch = require('node-fetch');
 const reply = require('../utils/reply.js');
 
 module.exports = {
-  name: 'emoji',
-  alias: ['jumbo'],
-  run: async (client, message, command, args, lang, guildInfo) => {
+    name: 'emoji',
+    alias: ['jumbo'],
+    run: async (client, message, command, args, lang, guildInfo) => {
 
-    for (guild of client.guilds.cache.values()) await guild.emojis.fetch();
-    let emoji = (args[0]?.split(':')[2]) ? client.emojis.resolve(args[0].split(':')[2].replace('>','')) : client.emojis.cache.find((e) => e.name === args[0]?.split(':')[1]);
-    if (!emoji) return reply(message, {content:lang.couldn_t_find_that_emoji}, true);
+        if (!args[0]) return reply(message, { content: lang.couldn_t_find_that_emoji }, true);
 
-    reply(message, {embeds:[{
-      title: lang.download_emoji,
-      url: emoji.url,
-      color: guildInfo.color,
-      image: { url: emoji.url }
-    }]});
+        let id = args[0].split(':')[2]?.replace('>', '') || args[0];
+        let emoji = id.startsWith(':') && id.endsWith(':') ? client.emojis.cache.find(e => e.name == id.slice(1, -1)) : client.emojis.cache.get(id);
+        let image = emoji?.url || client.rest.cdn.emoji(id, args[0].startsWith('<:') ? 'png' : 'gif');
 
-  }
+        if (id == args[0]) {
+            let gif = await fetch(image);
+            if (!gif.ok) image = `${image.slice(0, -3)}png`;
+        }
+
+        let res = await fetch(image);
+        if (!res.ok) return reply(message, { content: lang.couldn_t_find_that_emoji }, true);
+
+        reply(message, {
+            embeds: [{
+                title: lang.download_emoji,
+                url: image,
+                color: guildInfo.color,
+                image: { url: image }
+            }]
+        });
+
+    }
 }
