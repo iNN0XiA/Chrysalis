661e6b7984ed957cf449a511c19df5cf39113f0c
pLaYa iNN0XiA <118818738+iNN0XiA@users.noreply.github.com>
2022-12-14 01:32:18 -0500

    Update booru.js

--- src/commands/booru.js
+++ src/commands/booru.js
@@ -2,16 +2,16 @@ const fetchImage = require('../utils/booru/fetchImage.js');
 const parseQuery = require('../utils/booru/parseQuery.js');
 
 module.exports = {
-  name: 'booru',
-  alias: ['manebooru','brony'],
-  run: async (client, message, command, args, lang, guildInfo) => {
+    name: 'booru',
+    alias: ['manebooru', 'brony'],
+    run: async (client, message, command, args, lang, guildInfo) => {
 
-    let filter = guildInfo.modules.find((c) => c.name == 'booru').filter;
-    let query = await parseQuery(message, args);
+        let filter = guildInfo.modules.find((c) => c.name == 'booru').filter;
+        let query = await parseQuery(message, args);
 
-    query = query!='' ? `safe,${query}&filter_id=${filter}&per_page=50` : `safe&filter_id=${filter}&per_page=50`;
+        query = query != '' ? `safe,${query}&filter_id=${filter}&per_page=50` : `safe&filter_id=${filter}&per_page=50`;
 
-    fetchImage(client, query, message, guildInfo.color, 1, lang);
+        fetchImage(client, query, message, guildInfo.color, lang);
 
-  }
+    }
 }
