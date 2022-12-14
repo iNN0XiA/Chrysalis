8c312ddd92af211f5ddaa194e19330cb1540b134
pLaYa iNN0XiA <118818738+iNN0XiA@users.noreply.github.com>
2022-12-14 01:58:47 -0500

    Update boostEmbed.js

--- src/utils/embed/boostEmbed.js
+++ src/utils/embed/boostEmbed.js
@@ -1,15 +1,21 @@
-const { PermissionsBitField } = require('discord.js');
+const { PermissionsBitField } = require("discord.js");
 
-module.exports = async (client, user, level, channelID, color, lang) => {
-	if (!channelID) return;
-	let channel = await client.channels.fetch(channelID).catch(r=>{});
-	if (!channel) return;
-	if (!channel.permissionsFor(client.user.id).has(PermissionsBitField.Flags.ViewChannel)) return;
-	if (!channel.permissionsFor(client.user.id).has(PermissionsBitField.Flags.SendMessages)) return;
-	channel.send({content: `${user}`, embeds:[{
-		title: user.username,
-		description: lang.levelup(level),
-		color: color,
-		thumbnail: { url: user.displayAvatarURL({ forceStatic: true, size: 512 }) }
-	}]}).catch(r=>{});
+module.exports = async (member, guildInfo, channel, i) => {
+	let boost = guildInfo.modules.find((c) => c.name == 'boost');
+	if ((boost.enabled && boost.channel) || channel) {
+		channel ??= await member.guild.channels.fetch(boost.channel).catch(r=>{});
+		if (!channel) return;
+		let lang = require(`../../lang/${guildInfo.lang}.js`);
+		let message = {
+			content: (boost.message !== 'default' ? boost.message : lang.defaultValues.boost.message).replaceAll('{user}',member.user).replaceAll('{boostCount}',member.guild.premiumSubscriptionCount).replaceAll('{tier}',member.guild.premiumTier),
+			embeds:[{
+				title: (boost.title !== 'default' ? boost.title : lang.defaultValues.boost.title).replaceAll('{user}',member.user.username).replaceAll('{boostCount}',member.guild.premiumSubscriptionCount).replaceAll('{tier}',member.guild.premiumTier),
+				description: (boost.description !== 'default' ? boost.description : lang.defaultValues.boost.description).replaceAll('{user}',member.user).replaceAll('{boostCount}',member.guild.premiumSubscriptionCount).replaceAll('{tier}',member.guild.premiumTier),
+				thumbnail: { url: member.user.displayAvatarURL({ forceStatic: true, size: 512 }) },
+				color: member.guild.roles.premiumSubscriberRole?.color || 0xdb6de2 // Pink
+			}]
+		};
+		if (!channel.permissionsFor(member.client.user.id).has(PermissionsBitField.Flags.EmbedLinks)) message = lang.embed_links_permission_missing;
+		return i ? i.editReply(message) : channel.send(message).catch(r=>{});
+  }
 }
