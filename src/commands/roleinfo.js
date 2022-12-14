3311a6122d777f62dd7853b43ee19b181e4745f7
pLaYa iNN0XiA <118818738+iNN0XiA@users.noreply.github.com>
2022-12-14 01:40:19 -0500

    Update roleinfo.js

--- src/commands/roleinfo.js
+++ src/commands/roleinfo.js
@@ -1,34 +1,36 @@
 const reply = require('../utils/reply.js');
 
 module.exports = {
-  name: 'roleinfo',
-  alias: ['role-info','ri'],
-  run: async (client, message, command, args, lang, guildInfo) => {
+    name: 'roleinfo',
+    alias: ['role-info', 'ri'],
+    run: async (client, message, command, args, lang, guildInfo) => {
 
-    if (!args[0]) return reply(message, {content:lang.unkown_role}, true);
+        if (!args[0]) return reply(message, { content: lang.unkown_role }, true);
 
-    await message.guild.members.fetch();
-    await message.guild.roles.fetch();
+        await message.guild.members.fetch();
+        await message.guild.roles.fetch();
 
-    let requestedRole = message.guild.roles.cache.find(role => role.name.toLowerCase().includes(args.join(' ').toLowerCase()));
-    requestedRole ??= message.guild.roles.cache.find(role => role.id == args[0]);
-    requestedRole ??= message.guild.roles.cache.find(role => role.id == args[0].slice(3,-1));
+        let requestedRole = message.guild.roles.cache.find(role => role.name.toLowerCase().includes(args.join(' ').toLowerCase()));
+        requestedRole ??= message.guild.roles.cache.find(role => role.id == args[0]);
+        requestedRole ??= message.guild.roles.cache.find(role => role.id == args[0].slice(3, -1));
 
-    if (!requestedRole) return reply(message, {content:lang.unkown_role}, true);
+        if (!requestedRole) return reply(message, { content: lang.unkown_role }, true);
 
-    let created = Math.trunc(requestedRole.createdTimestamp / 1000);
+        let created = Math.trunc(requestedRole.createdTimestamp / 1000);
 
-    reply(message, {embeds:[{
-      description: `__**${lang.role_info}**__`,
-      color: requestedRole.color,
-      fields: [
-        { name: lang.name, value: `${requestedRole}` },
-        { name: lang.role_id, value: `${requestedRole.id}` },
-        { name: lang.color, value: `${requestedRole.hexColor}` },
-        { name: lang.member_count, value: `${requestedRole.members.size}` },
-        { name: lang.date_created, value: `<t:${created}:F> (<t:${created}:R>)` }
-      ]
-    }]});
+        reply(message, {
+            embeds: [{
+                description: `__**${lang.role_info}**__`,
+                color: requestedRole.color,
+                fields: [
+                    { name: lang.name, value: `${requestedRole}` },
+                    { name: lang.role_id, value: `${requestedRole.id}` },
+                    { name: lang.color, value: `${requestedRole.hexColor}` },
+                    { name: lang.member_count, value: `${requestedRole.members.size}` },
+                    { name: lang.date_created, value: `<t:${created}:F> (<t:${created}:R>)` }
+                ]
+            }]
+        });
 
-  }
+    }
 }
