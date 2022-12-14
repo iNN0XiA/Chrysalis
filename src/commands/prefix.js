feb7e33e3c1dd66d9e9f06c78265d3c1c622b53b
pLaYa iNN0XiA <118818738+iNN0XiA@users.noreply.github.com>
2022-12-14 01:39:12 -0500

    Update prefix.js

--- src/commands/prefix.js
+++ src/commands/prefix.js
@@ -1,46 +1,48 @@
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const connectToDatabase = require('../utils/connectToDatabase.js');
+const reply = require('../utils/reply.js');
+const emojis = require('../emojis.js');
 
 module.exports = {
-  name: 'prefix',
-  alias: ['setprefix','changeprefix','prefixset','set-prefix','change-prefix'],
-  admin: true,
-  run: (client, message, command, args, lang, guildInfo) => {
+    name: 'prefix',
+    alias: ['setprefix', 'changeprefix', 'prefixset', 'set-prefix', 'change-prefix'],
+    admin: true,
+    run: async (client, message, command, args, lang, guildInfo) => {
 
-    if (!args[0]) return message.channel.send(lang.the_current_prefix_is.replace('{0}', guildInfo.prefix))
+        if (!args[0]) return message.channel.send(lang.the_current_prefix_for_this_server_is(`**${guildInfo.prefix}**`));
 
-    message.channel.send(lang.change_prefix_to.replace('{0}', args[0])).then(confMsg => {
-      confMsg.react('✅');
-      confMsg.react('❌');
-      let filter = (reaction, user) => user.id === message.author.id;
-      let collector = confMsg.createReactionCollector({filter,  time: 15000 });
-      collector.on('collect', r => {
-        switch (r.emoji.name) {
-          case '✅':
-            updatePrefix(message, args[0], lang);
-            confMsg.delete();
-          break;
-          case '❌':
-            confMsg.delete();
-            break;
-        }
-      });
-      collector.on('end', (collected, reason) => {
-        if (reason == 'time') {
-          try {
-            confMsg.delete();
-            message.delete();
-          } catch (e) {}
-        }
-      });
-    });
-  }
-}
-
-async function updatePrefix(message, newPrefix, lang) {
-  let db = await connectToDatabase();
-  let guilds = db.db('chrysalis').collection('guilds');
-  let guild = await guilds.findOne({id: message.guild.id});
-  await guilds.updateOne({id: message.guild.id},{ $set: { prefix: newPrefix}});
-  db.close();
-  message.channel.send(lang.prefix_was_changed_to.replace('{0}', newPrefix));
+        reply(message, {
+            content: lang.change_prefix_to(args[0]),
+            components: [new ActionRowBuilder().addComponents([
+                new ButtonBuilder({
+                    customId: `prefix-${args[0]}`,
+                    emoji: emojis.PinkiePieYes,
+                    style: ButtonStyle.Success
+                }),
+                new ButtonBuilder({
+                    customId: 'prefix-dismiss',
+                    emoji: emojis.PinkiePieNo,
+                    style: ButtonStyle.Danger
+                })
+            ])]
+        }).then(confMsg => {
+            let collector = confMsg.createMessageComponentCollector({ filter: (i) => i.member.id === message.member.id, time: 15_000 });
+            collector.on('collect', async (i) => {
+                collector.stop(i.customId === 'prefix-dismiss' ? 'time' : 'new prefix!!!');
+                if (i.customId === `prefix-${args[0]}`) {
+                    let db = await connectToDatabase();
+                    let guilds = db.db('chrysalis').collection('guilds');
+                    await guilds.updateOne({ id: message.guild.id }, { $set: { prefix: args[0] } });
+                    db.close();
+                    i.update({ content: lang.prefix_was_changed_to(args[0]), components: [] });
+                }
+            });
+            collector.on('end', (collected, reason) => {
+                if (reason == 'time') try {
+                    confMsg.delete();
+                    message.delete();
+                } catch (e) { }
+            });
+        });
+    }
 }
