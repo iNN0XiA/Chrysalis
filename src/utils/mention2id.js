811e5cf51f529fee791aff52177c22c25ab0b489
pLaYa iNN0XiA <118818738+iNN0XiA@users.noreply.github.com>
2022-12-18 15:51:26 -0500

    Delete src/utils directory

--- src/utils/mention2id.js
+++
@@ -1,6 +0,0 @@
-module.exports = (mention) => {
-  if (!mention) return;
-  if (mention.startsWith('<@!')) return mention.slice(3,-1);
-  if (mention.startsWith('<@')) return mention.slice(2,-1);
-  return mention;
-}
