4a43f845930aaa74062fd336294df386f41af303
pLaYa iNN0XiA <118818738+iNN0XiA@users.noreply.github.com>
2022-12-20 07:00:22 -0500

    Update formatLeaderboard.js

--- src/utils/embed/formatLeaderboard.js
+++ src/utils/embed/formatLeaderboard.js
@@ -1,41 +1,42 @@
+
 module.exports = async (users, guild, guildInfo, lang) => {
-    let lb = ''
-    let highscores = users.sort((a, b) => (a.xp < b.xp) ? 1 : -1);
-    for (i in highscores.slice(0, 10)) {
-        lb += `${getNumberEmoji(+i + 1)} ► <@${highscores[i].id}>
-          ${lang.level}: \`${Math.trunc((Math.sqrt(5) / 5) * Math.sqrt(highscores[i].xp))}\`
+  let lb = ''
+  let highscores = users.sort((a, b) => (a.xp < b.xp) ? 1 : -1);
+  for (i in highscores.slice(0,10)) {
+    lb+=`${getNumberEmoji(+i+1)} ► <@${highscores[i].id}>
+          ${lang.level}: \`${Math.trunc((Math.sqrt(5)/5)*Math.sqrt(highscores[i].xp))}\`
           XP: \`${highscores[i].xp}\`\n`;
-    }
-    return {
-        title: lang.leaderboard_title,
-        description: lb,
-        color: guildInfo.color,
-        thumbnail: { url: guild.iconURL({ size: 1024 }) }
-    };
+  }
+  return {
+    title: lang.leaderboard_title,
+    description: lb,
+    color: guildInfo.color,
+    thumbnail: { url: guild.iconURL({size:1024}) }
+  };
 }
 
 
 function getNumberEmoji(n) {
-    switch (n) {
-        case 1:
-            return ':first_place:'
-        case 2:
-            return ':second_place:'
-        case 3:
-            return ':third_place:'
-        case 4:
-            return ':four:'
-        case 5:
-            return ':five:'
-        case 6:
-            return ':six:'
-        case 7:
-            return ':seven:'
-        case 8:
-            return ':eight:'
-        case 9:
-            return ':nine:'
-        case 10:
-            return ':keycap_ten:'
-    }
+  switch (n) {
+    case 1:
+    return ':first_place:'
+    case 2:
+    return ':second_place:'
+    case 3:
+    return ':third_place:'
+    case 4:
+    return ':four:'
+    case 5:
+    return ':five:'
+    case 6:
+    return ':six:'
+    case 7:
+    return ':seven:'
+    case 8:
+    return ':eight:'
+    case 9:
+    return ':nine:'
+    case 10:
+    return ':keycap_ten:'
+  }
 }
